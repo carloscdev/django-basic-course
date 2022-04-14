a3c33fd6e2985c23c64613a5b1e24733b004f546
Carlos Córdova <ccordova.zosma@gmail.com>
2022-04-13 22:46:38 -0500

    finish Django Basic Course

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from .views import IndexView, DetailView, ResultView
 
+app_name = "polls"
 urlpatterns = [
-  path('', views.index, name='index'),
+  path('', IndexView.as_view(), name='index'),
+  path('<int:pk>', DetailView.as_view(), name='detail'),
+  path('<int:pk>/results', ResultView.as_view(), name='results'),
+  path('<int:question_id>/vote', views.vote, name='vote'),
 ]
